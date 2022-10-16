025093a95e23b9078e384990dbfef982ad1df8cd
Windla <63698964+Windla@users.noreply.github.com>
2022-10-16 10:55:31 +0800

    v2.2.2

--- osuWRS.py
+++ osuWRS.py
@@ -8,19 +8,19 @@ import win32api
 import win32con
 from threading import Thread
 
-quick_start = 1  # 快速启动  0：关闭 1:开启 | 默认 0
-overtime = 5  # 连接ppy服务器超时时间 | 默认 5
-version = "2.2.1"  # 当前OsuWRS版本号
-OsuWRS_path = os.getcwd()  # 获取OsuWRS工作目录
+QUICK_START = 1  # 快速启动  0：关闭 1:开启 | 默认 0
+OVERTIME = 5  # 连接ppy服务器超时时间 | 默认 5
+VERSION = "2.2.1"  # 当前osuWRS版本号
+osuWRS_path = os.getcwd()  # 获取osuWRS工作目录
 stop_threads = False  # 全局变量 virtual_key 线程运行&退出 | True: 不进行按键模拟
 
-os.system("title OsuWRS By -Windla- (Bilibili)")
-print("[!] OsuWRS作者: B站 @-Windla-")
-print("[!] OsuWRS版本:", version)
+os.system("title osuWRS By -Windla- (Bilibili)")
+print("[!] osuWRS作者: B站 @-Windla-")
+print("[!] osuWRS版本:", VERSION)
 
 
 # bg刷新检测
-def get_bg_num():
+def bg_num_get():
     num = len(glob.glob(bgPath + "\\*.jpg"))
     return num
 
@@ -48,7 +48,7 @@ def virtual_key():
 
 
 # 遍历更改bg文件名
-def rename(path, name):
+def bg_rename(path, name):
     i = 1
     for file in os.listdir(path):
         os.rename(os.path.join(path, file), os.path.join(path, name + str(i)) + ".jpg")
@@ -59,10 +59,10 @@ def rename(path, name):
 def bg_check():
     i = 1
     while i != bg_num + 1:
-        if not os.path.exists(OsuWRS_path + "\\bg\\bg_" + str(i) + ".jpg"):
+        if not os.path.exists(osuWRS_path + "\\bg\\bg_" + str(i) + ".jpg"):
             print("[-] 状态: 重新初始化bg文件名 | bg_" + str(i) + "丢失")
-            rename(OsuWRS_path + "\\bg", "pre_bg_")
-            rename(OsuWRS_path + "\\bg", "bg_")
+            bg_rename(osuWRS_path + "\\bg", "pre_bg_")
+            bg_rename(osuWRS_path + "\\bg", "bg_")
             break
         i += 1
 
@@ -79,12 +79,12 @@ def bg_replace():
 
     os.system('echo y|cacls ' + bgPath + ' /t /p everyone:f > nul')
     os.system('md ' + Path + '\\Data\\bg_temp > nul')
-    os.system('copy "' + OsuWRS_path + '\\bg\\*.jpg" "' + Path + '\\Data\\bg_temp" > nul')
+    os.system('copy "' + osuWRS_path + '\\bg\\*.jpg" "' + Path + '\\Data\\bg_temp" > nul')
 
     num = 1
-    for Osu_bg in glob.glob(bgPath + "\\" + "*.jpg"):
-        os.remove(Osu_bg)
-        os.rename(bgTempPath + "\\bg_" + str(num) + ".jpg", Osu_bg)
+    for osu_bg in glob.glob(bgPath + "\\" + "*.jpg"):
+        os.remove(osu_bg)
+        os.rename(bgTempPath + "\\bg_" + str(num) + ".jpg", osu_bg)
         if num == bg_num:
             break
         num += 1
@@ -97,11 +97,11 @@ def bg_replace():
 def thread_main():
     i = 0
     e = 0  # 错误计数
-    n = get_bg_num()
+    n = bg_num_get()
     global stop_threads
     print("[!] 提示: 请确保你的鼠标焦点位于osu!上, 不要移动!")
     while n != bg_num:
-        n = get_bg_num()
+        n = bg_num_get()
         time.sleep(0.01)  # 越小消耗越大但越精准
         if i != n:
             print("[+] 进度: 已获取" + str(n) + "张服务器bg图")
@@ -118,9 +118,9 @@ def thread_main():
             os.system("taskkill /F /IM osu!.exe > nul")
             with open("error.log", "w") as file:
                 file.write('[Error] 不要超过ppy的服务器上限! 目前是最高' + str(n) + '张可替换的bg图 | 或者检查您的网络')
-            win32api.ShellExecute(0, 'open', OsuWRS_path + "\\bg", '', '', 1)
+            win32api.ShellExecute(0, 'open', osuWRS_path + "\\bg", '', '', 1)
             time.sleep(2)
-            win32api.ShellExecute(0, 'open', OsuWRS_path + "\\error.log", '', '', 1)
+            win32api.ShellExecute(0, 'open', osuWRS_path + "\\error.log", '', '', 1)
             exit()
     # 声明退出virtual_key线程
     stop_threads = True
@@ -144,9 +144,9 @@ def thread_main():
 
 # ------------------------------初始化 | 配置文件读取---------------------------------
 # bg文件夹 | bg数量识别
-if not os.path.exists(OsuWRS_path + "\\bg"):
-    os.mkdir(OsuWRS_path + "\\bg")
-bg_num = len(glob.glob(OsuWRS_path + "\\bg\\*.*"))
+if not os.path.exists(osuWRS_path + "\\bg"):
+    os.mkdir(osuWRS_path + "\\bg")
+bg_num = len(glob.glob(osuWRS_path + "\\bg\\*.*"))
 if bg_num == 0:
     print("[-] Error: 请在运行目录下的bg文件夹内放入待替换图片(只接受jpg/png格式 | 不建议超过10张)")
     time.sleep(5)
@@ -158,33 +158,33 @@ try:
         Path = f.read()  # osu 路径
         f.close()
     if Path.find("Error") != -1 or Path.find("osu!") == -1:
-        win32api.ShellExecute(0, 'open', OsuWRS_path + "\\config.ini", '', '', 1)
+        win32api.ShellExecute(0, 'open', osuWRS_path + "\\config.ini", '', '', 1)
         exit()
 except FileNotFoundError:
     with open("config.ini", "w") as f:
-        f.write('Error: 请将该行替换成你osu!根目录的绝对路径, 并重新打开OsuWRS')
-    win32api.ShellExecute(0, 'open', OsuWRS_path + "\\config.ini", '', '', 1)
+        f.write('Error: 请将该行替换成你osu!根目录的绝对路径, 并重新打开osuWRS')
+    win32api.ShellExecute(0, 'open', osuWRS_path + "\\config.ini", '', '', 1)
     exit()
 osuPath = Path + "\\osu!.exe"  # osu.exe 路径
 bgPath = Path + "\\Data\\bg"  # bg文件夹 路径
 bgTempPath = Path + "\\Data\\bg_temp"  # 跨驱动器的解决方案
 
 # 快速启动
-if quick_start == 1:
+if QUICK_START == 1:
     win32api.ShellExecute(0, 'open', osuPath, '', '', 1)
 
 # 获取ppy数据&本地数据
 while True:
     try:
         print("[+] 状态: 正在连接osu!服务器")
-        ppyUrl = requests.get("https://osu.ppy.sh/web/osu-getseasonal.php", timeout=overtime).text.encode()  # ppy数据
+        ppyUrl = requests.get("https://osu.ppy.sh/web/osu-getseasonal.php", timeout=OVERTIME).text.encode()  # ppy数据
         print("[+] 状态: 获取bg信息成功")
         break
-    except requests.exceptions.RequestException:  # 蚌埠住了
+    except requests.exceptions.RequestException:
         print("[-] 连接osu!服务器超时")
 while True:
     try:
-        with open("bg.php", 'r') as f:  # 原数据
+        with open("bg.php", 'r') as f:
             ppyUrlOld = (f.read()).encode()
             f.close()
         break
@@ -204,7 +204,7 @@ if ppyUrl != ppyUrlOld:  # seasonal background 更新后开始进行 bg更新
     # 对osu启动的检测
     getnum = 0
     while getnum == 0:
-        getnum = get_bg_num()
+        getnum = bg_num_get()
         time.sleep(0.1)
     print("[+] 进度: 成功启动osu!")
     time.sleep(3)
@@ -220,5 +220,5 @@ if ppyUrl != ppyUrlOld:  # seasonal background 更新后开始进行 bg更新
 else:
     print("[+] 状态: 无更新!")
     time.sleep(1)
-    if quick_start != 1:
+    if QUICK_START != 1:
         win32api.ShellExecute(0, 'open', osuPath, '', '', 1)
